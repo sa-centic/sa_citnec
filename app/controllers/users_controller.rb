84e26d65f8463aa502c6cf69d2c380aa6e613281
sa-centic <sa@centic.dk>
2022-04-11 10:52:23 +0200

    Added styling to courses views, and added user type filters, to ransack _search_form.slim

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :roles, courses_attributes: [:id, :course_name, :_destroy])
+    params.require(:user).permit(:first_name, :last_name, :email, courses_attributes: [:id, :course_name, :_destroy], type_ids: [])
   end
 
 
