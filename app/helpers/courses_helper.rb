c9d3951afb9add6d675d73004b9d7444a5fbb5d6
sa-centic <sa@centic.dk>
2022-04-22 12:05:16 +0200

    Made wysiwyg work

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,10 +1,20 @@
 module CoursesHelper
 
   def render_trix_with_vars(content)
-    content.print("%{course_name}%", @course.course_name)
-
-
+    content.gsub!("%course_name%", + @course.course_name)
+    content.gsub!("%course_description%", @course.course_description)
+    content.gsub!("%max_coursetakers%", @course.max_coursetakers.to_s)
+    content.gsub!("%coursetakers%", list_of_coursetakers(@course.coursetakers))
+    puts content
+    content
   end
 
+  def list_of_coursetakers(coursetakers)
+    result = "<ul>"
+    coursetakers.each do |ct|
+      result += "<li>#{ct.full_name}</li>"
+    end
+    result += "</ul>"
+  end
 
 end
